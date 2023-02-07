52e3b6922fcd8464ab8f146ac73f78a4a70de13c
Oleh Ivsv <you@example.com>
2023-02-07 20:22:38 +0200

    Add cart api to handle requests related to shopping cart

---
+++ src/redux/cart/cartApiSlice.js
@@ -0,0 +1,39 @@
+import { api } from "../auth/api";
+
+
+export const cartApiSlice = api.injectEndpoints({
+    endpoints: builder => ({
+        /* Get cart by customer id */
+        cart: builder.query({
+            query: params => ({
+                url: `cart/${params.customerId}`,
+                method: 'GET'
+            }),
+            providesTags:['Cart']    
+        }),
+        /* Removes item from cart */
+        removeItem: builder.mutation({
+            query: params => ({
+                url: `cart/remove-item/${params.customerId}?productId=${params.productId}`,
+                method: 'PUT',
+                body: {}
+            }),
+            invalidatesTags:['Cart']
+        }),
+        /* Updates item */
+        updateItem: builder.mutation({
+            query: params => ({
+                url: `cart/update-item/${params.customerId}`,
+                method: 'PUT',
+                body: {...params.item}
+            }),
+            invalidatesTags: ['Cart']
+        })
+    })
+});
+
+export const { 
+    useCartQuery, 
+    useRemoveItemMutation,
+    useUpdateItemMutation,
+} = cartApiSlice;
