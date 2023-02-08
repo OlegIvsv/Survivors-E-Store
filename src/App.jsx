41091ae6b1c7dcd94c365c26f60704ba38aec837
Oleh Ivsv <you@example.com>
2023-02-08 18:07:26 +0200

    Update app to use routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css';
 import ShoppingCart from './components/ShoppingCart';
 import TopMenu from './components/TopMenu';
 import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App bg-gray-200 flex flex-col min-h-screen justify-between">
-      <TopMenu/>
-      <ShoppingCart/>
-      <Footer/>
+      <BrowserRouter>
+        <TopMenu />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="cart" element={<ShoppingCart />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
