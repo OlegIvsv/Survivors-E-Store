5e068cad1964775ba1ed756347533dc5f5145017
Oleh Ivsv <you@example.com>
2023-02-20 12:28:40 +0200

    Install all the necessary packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,24 @@ module.exports = {
   daisyui: {
     themes: [],
   },
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'x-green': '#0cbaba',
-        'x-dark-green': '#102e44',
-        'x-red': '#db2763',
-        'x-blue': '#01baef',
-        'x-white': '#e0f8ff'
+      colors: {
+        "x-green": "#0cbaba",
+        "x-dark-green": "#102e44",
+        "x-red": "#db2763",
+        "x-blue": "#01baef",
+        "x-white": "#e0f8ff",
       },
       fontSize: {
-        xxs: ['0.5rem', '0.666rem'],
-      }
+        xxs: ["0.5rem", "0.666rem"],
+      },
     },
   },
   plugins: [
-    require("daisyui")
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+    require("@tailwindcss/line-clamp"),
   ],
-}
+};
