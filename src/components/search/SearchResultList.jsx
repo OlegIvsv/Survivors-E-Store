88ce6f4f12e1d40aca3366cf0c3c356df301654c
Oleh Ivsv <you@example.com>
2023-02-20 12:49:54 +0200

    Add SearchResultList which represents a list of found products

---
+++ src/components/search/SearchResultList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { displayOptionSelector } from '../../redux/search/searchSlice';
+import { Pagination } from './Pagination';
+import { ProductCell, ProductRow } from './Product';
+
+
+export function SearchResultList({products}) {
+  const display = useSelector(displayOptionSelector);
+
+  return (
+    <div className="basis-3/4 p-1">
+      <div className="flex flex-row flex-wrap">
+        {products.map((product, index) => {
+          if (display === "cells")
+            return (
+              <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 px-3 py-2">
+                <ProductCell key={index} product={product} />
+              </div>
+            );
+          return (
+            <div className="w-full px-3 py-1 h-60">
+              <ProductRow key={index} product={product} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-center py-4">
+        <Pagination numberOfPages={10} />
+      </div>
+    </div>
+  );
+}
