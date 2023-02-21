59611a0407ab004159bda64ac0ff2bcb4e8e126a
Oleh Ivsv <you@example.com>
2023-02-21 18:01:40 +0200

    Refactor auth slise. Move selectors to separate file

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,44 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-/** 
- * Init state for testsing! 
- * Should be replaced with emopty one when authentication page is created!
-*/
-const initialState = { 
-    name: "User_1", 
-    id: "",
-    login: 'login',
-    password: 'password',
-    token: ""
+const initialState = {
+  name: "",
+  id: "",
+  login: "",
+  password: "",
+  token: "",
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers:{
-        setCredentials: (state, action) => {
-            const { name, token, id, password, login } = action.payload;
-            state.name = name;
-            state.token = token;
-            state.id = id;
-            state.password = password;
-            state.login = login;
-        },
-        setId: (state, action) => {
-            state.id = action.payload.id;
-        },
-        logout: (state) => {
-            state.name = '';
-            state.token = '';
-            state.id = '';
-            state.password = '';
-            state.login = '';
-        }
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      const { name, token, id, password, login } = action.payload;
+      state.name = name;
+      state.token = token;
+      state.id = id;
+      state.password = password;
+      state.login = login;
+    },
+    logout: (state) => {
+      state.name = "";
+      state.token = "";
+      state.id = "";
+      state.password = "";
+      state.login = "";
     }
+  },
 });
 
 export default authSlice.reducer;
 export const { setCredentials, logout, setId } = authSlice.actions;
-
-export const authStateSelector = (state)  => state.auth;
-export const authUserIdSelector = (state)  => state.auth.id; 
---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,3 @@
+export const authStateSelector = (state) => state.auth;
+export const authUserIdSelector = (state) => state.auth.id;
+export const tokenSelector = (state) => state.auth.token;
